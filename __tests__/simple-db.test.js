901c11df77fa9e53aeed755967332c031b4993d2
Julius Franklin <jjfranklin88@outlook.com>
2022-02-14 17:52:05 -0500

    get by id, save test passing

--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises');
 const path = require('path');
+const SimpleDb = require('../lib/simple-db');
 
 const { CI, HOME } = process.env;
 const BASE_DIR = CI ? HOME : __dirname;
@@ -12,8 +13,26 @@ describe('simple database', () => {
     await fs.mkdir(TEST_DIR, { recursive: true });
   });
 
-  it('needs a first test...', async () => {
+  it('should save a file', async () => {
+    const simpleDb = new SimpleDb(TEST_DIR);
+    const newFile = {
+      name: 'newfile.txt',
+    };
+    // const fileWithId = { id: expect.any(String), name: 'newfile.txt' };
 
+    return simpleDb.save(newFile)
+      .then(() => simpleDb.get(newFile.id))
+      .then((file) => expect(file).toEqual({ ...newFile, id: expect.any(String) }));
   });
+
+  it('should throw an error when no id exists', async () => {
+    const simpleDb = new SimpleDb(TEST_DIR);
+    const id = 'no id ';
+
+    return simpleDb.get(id)
+      .then((file) => expect(file).toEqual(null));
+  });
+
+
 
 });
